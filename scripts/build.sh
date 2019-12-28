84952e2a456de630b080c57d4ed7e645fdb7d6f1
Michael Bright <1880109+mjbright@users.noreply.github.com>
2019-12-28 20:55:33 +0100

    Added scripts

---
+++ scripts/build.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+DATE_VERSION=$(date +%Y-%b-%d_%02Hh%02Mm%02S)
+
+PROMPTS=0
+
+function die {
+    echo "$0: die - $*" >&2
+    exit 1
+}
+
+function press {
+    echo $*
+    echo "Press <return>"
+    read DUMMY
+    [ "$DUMMY" = "q" ] && exit 0
+    [ "$DUMMY" = "Q" ] && exit 0
+}
+
+function build {
+    # builds dynamic binary:
+    # go build -o k8scenario k8scenario.go || exit 1
+    # builds static binary:
+
+    set -x
+    sed -i.bak \
+	    -e "s/__DATE_VERSION__.*=.*/__DATE_VERSION__=\"$DATE_VERSION\"/g" \
+	    -e "s?__DEFAULT_PUBURL__.*=.*?__DEFAULT_PUBURL__=\"$DEFAULT_PUBURL\"?g" \
+	    k8scenario.go 
+
+    time CGO_ENABLED=0 go build -a -o bin/$K8SCENARIO_BINARY k8scenario.go || exit 1
+}
+
+function install_build {
+    mkdir -p BAK/
+    ls -altrh bin/k8scenario 
+    cp -a k8scenario.go BAK/k8scenario.go.$DATE_VERSION
+
+    if [ -d ~/usr/bin_lin64/ ]; then
+        cp -a bin/$K8SCENARIO_BINARY ~/usr/bin_lin64/${K8SCENARIO_BINARY}.$DATE_VERSION
+
+        ls -altrh ~/usr/bin_lin64/${K8SCENARIO_BINARY}.$DATE_VERSION
+
+        # Copy file from symlink: (no -a):
+        cp  /home/mjb/usr/bin_lin64/kubectl ./bin/
+    else
+        if [ -d ~/BINARIES/bin_lin64/ ]; then
+            cp -a bin/$K8SCENARIO_BINARY ~/BINARIES/bin_lin64/${K8SCENARIO_BINARY}.$DATE_VERSION
+            ls -altrh ~/BINARIES/bin_lin64/${K8SCENARIO_BINARY}.$DATE_VERSION
+
+            # Copy file from symlink: (no -a):
+            cp  /home/mjb/BINARIES/bin_lin64/kubectl ./bin/
+        fi
+    fi
+}
+
+function docker_build_and_push {
+
+    [ $PROMPTS -ne 0 ] && press "About to build docker image"
+
+
+    docker build -t mjbright/k8scenario:latest .
+
+    if [ -z "$TAG" ];then
+        TAG="latest"
+        docker push mjbright/k8scenario:latest
+    else
+        TAG="latest"
+        docker tag mjbright/k8scenario:latest mjbright/k8scenario:$TAG
+        docker push mjbright/k8scenario:latest
+        docker push mjbright/k8scenario:$TAG
+    fi
+}
+
+function CLEAN_k8scenario {
+    echo "---- Deleting k8scenario pod if present:"
+    kubectl get pod |& grep k8scenario && kubectl delete pod k8scenario
+
+    echo "---- Deleting scenario namespaces if present:"
+    NAMESPACES=$(kubectl get ns |& grep ^scenario | awk '{ print $1; }')
+    if [ ! -z "$NAMESPACES" ]; then
+        for NAMESPACE in $NAMESPACES; do
+            echo kubectl delete ns/$NAMESPACE
+            kubectl delete ns/$NAMESPACE
+        done
+    fi
+
+    #kubectl get ns |& grep ^scenario && kubectl delete ns $(kubectl get ns | grep ^scenario | awk '{ print $1; }')
+}
+
+function run_k8scenario {
+    sed "s/__TAG__/$TAG/g" < k8scenario.template.yaml > k8scenario.yaml
+    sed "s/__TAG__/$TAG/g" < k8scenario_menu.template.yaml > k8scenario_menu.yaml
+
+    # CLEANUP:
+    CLEAN_k8scenario
+
+    #kubectl create -f k8scenario.yaml
+    kubectl create -f k8scenario_menu.yaml
+
+    while kubectl get pods k8scenario | grep Running; do
+	echo "Waiting for pod/k8scenario to be Running"
+        sleep 2
+        kubectl get pods k8scenario
+    done
+    kubectl get pods k8scenario
+
+    #while ! kubectl logs pod/k8scenario | grep -v "Detect doesn't exists"; do
+    #done
+    echo "Waiting for pod/k8scenario logs ..."
+    sleep 5
+    kubectl logs pod/k8scenario
+
+    while true; do
+        echo "---- $(date) ----------------------"
+        LAST_SCENARIO=$(kubectl get ns | awk '/^scenario/ { print $1; }' | tail -1)
+        kubectl -n $LAST_SCENARIO get all
+	sleep 5
+    done
+}
+
+[ ! -f .setup.rc ] && die "No .setup.rc in $PWD"
+. .setup.rc
+
+[ -z "$DEFAULT_PUBURL" ] && die "\$DEFAULT_PUBURL not set in .setup.rc"
+[ -z "$DEFAULT_PUBDIR" ] && die "\$DEFAULT_PUBDIR not set in .setup.rc"
+[ ! -d "$DEFAULT_PUBDIR" ] && die "No such \$DEFAULT_PUBDIR dir as <$DEFAULT_PUBDIR>"
+
+# Add TAG:
+TAG=""
+
+while [ ! -z "$1" ]; do
+    case $1 in
+        -k8s|-run)
+	      run_k8scenario; exit $?;;
+        *)    TAG=$1;;
+    esac
+    shift
+done
+
+build
+install_build
+#docker_build_and_push
+
+if [ $PROMPTS -ne 0 ]; then
+    press "About to run k8scenario"
+
+    run_k8scenario
+fi
+
+
+
---
+++ scripts/update_scenarii.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+
+. .setup.rc
+
+BASE_DIR=$(readlink -f $(dirname $0)/..)
+SCENARII_DIR=$(dirname $0)/../SCENARII
+
+URL_BASE=$DEFAULT_PUBURL
+PUB_URL_DIR=$DEFAULT_PUBDIR
+URL_SCENARII_DIR=$PUB_URL_DIR/static/k8scenarii
+URL_BIN_DIR=$PUB_URL_DIR/static/bin
+#REPO_DIR=$DEFAULT_PUBDIR
+#PUB_URL_DIR=$REPO_DIR/static/k8scenarii/
+
+WEB_UPLOAD=1
+WEB_UPLOAD=0
+
+die() {
+    echo "$0: die - $*" >&2
+    exit 1
+}
+
+create_zips() {
+    #cd $SCENARII_DIR/static/k8scenarii
+    cd $SCENARII_DIR/
+    echo "[$PWD] Creating zips ..."
+
+    for scenario in scenario[0-9]*/; do
+        scenario=${scenario%/}
+
+        # Skip if not scenario[0-9]+:
+        [ ${scenario%[0-9]} = $scenario ] && continue
+
+        [ ! -d $scenario ] && die "Expected directory <$scenario> in $SCENARII_DIR"
+
+	#set -x
+        [ -f ${scenario}.zip ] && rm -f ${scenario}.zip
+        zip -r9 ${scenario}.zip ${scenario}/ -x '*/EXCLUDE_*' 2>&1 >/dev/null
+
+        [ ! -f ${scenario}.zip ] && die "Failed to create zip <${scenario}.zip>"
+        [ ! -d $URL_SCENARII_DIR ] && mkdir -p $URL_SCENARII_DIR
+        [ ! -d $URL_BIN_DIR ]      && mkdir -p $URL_BIN_DIR
+        cp -a ${scenario}.zip index.list $URL_SCENARII_DIR/
+        cp -a ../bin/$K8SCENARIO_BINARY $URL_BIN_DIR/k8scenario
+	#set +x
+    done
+    ls -altr *.zip
+
+    #cd -
+    cd $BASE_DIR
+    echo "[$PWD] Creating zips ... DONE"
+    echo "Zips copied to $PUB_URL_DIR/"
+}
+
+upload_zips() {
+    #cd $REPO_DIR
+    cd $PUB_URL_DIR
+    echo "[$PWD] Uploading zips to github ..."
+
+    #git add static/k8scenarii/
+    git add .
+    git commit -m "Adding latest k8scenarii"
+    git push
+
+    #cd -
+    cd $BASE_DIR
+    echo "[$PWD] Uploading zips to github ... DONE"
+}
+
+#DOWNLOAD="wget --no-check-certificate --no-cache --no-cookies --post-data='action=purge'"
+DOWNLOAD="wget --no-check-certificate --no-cache --no-cookies -q -O -"
+DOWNLOAD="curl -sL -o -"
+
+check_zips() {
+    cd $SCENARII_DIR
+
+    for scenario in scenario*/; do
+        scenario=${scenario%/}
+
+        # Skip if not scenario[0-9]+:
+        [ ${scenario%[0-9]} = $scenario ] && continue
+
+        [ ! -d $scenario ] && die "Expected directory <$scenario> in $SCENARII_DIR"
+
+        cksum=$(cksum < ${scenario}.zip)
+        URL=$URL_BASE/${scenario}.zip
+
+	echo; echo "==== Getting zip file from $URL"
+        wcksum=$($DOWNLOAD $URL | cksum)
+        while [ "$cksum" != "$wcksum" ]; do
+            SLEEP=10
+            echo "Sleeping $SLEEP secs [waiting for ${scenario}.zip file to be updated <local> $cksum != <online> $wcksum]"
+	    sleep $SLEEP
+            wcksum=$($DOWNLOAD $URL | cksum)
+	done
+        echo "$scenario: cksum OK $cksum == $wcksum [$URL]"
+
+        #if [ "$cksum" != "$wcksum" ]; then
+        #    echo "$scenario: $cksum != $wcksum [$URL]"
+        #else
+        #    echo "$scenario: cksum OK $cksum == $wcksum [$URL]"
+        #fi
+    done
+
+    #cd -
+    cd $BASE_DIR
+}
+
+function rebuild_index {
+    cd $SCENARII_DIR
+    echo "[$PWD] Rebuilding index"
+
+    cp /dev/null index.list
+    for scenario in scenario[0-9]*/; do
+        scenario=${scenario%/}
+
+        # Skip if not scenario[0-9]+:
+        [ ${scenario%[0-9]} = $scenario ] && continue
+
+        echo "# $scenario: "
+        [ -f ${scenario}.fix/README.txt ] && cat ${scenario}.fix/README.txt
+	NUM=${scenario#scenario}
+	NUM=${NUM%/}
+	echo ${NUM} >> index.list
+    done > INDEX.md
+
+    #cd -
+    cd $BASE_DIR
+    echo "[$PWD] Rebuilding index ... DONE"
+}
+
+[ ! -d $SCENARII_DIR ] && die "No such scenario dir <$SCENARII_DIR>"
+
+[ ! -f .setup.rc ] && die "No .setup.rc in $PWD"
+. .setup.rc
+
+[ -z "$DEFAULT_PUBURL" ] && die "\$DEFAULT_PUBURL not set in .setup.rc"
+[ -z "$DEFAULT_PUBDIR" ] && die "\$DEFAULT_PUBDIR not set in .setup.rc"
+[ ! -d "$DEFAULT_PUBDIR" ] && die "No such \$DEFAULT_PUBDIR dir as <$DEFAULT_PUBDIR>"
+
+while [ ! -z "$1" ]; do
+    case $1 in
+	# Run server as:
+	#    ./SCENARII/local_server.sh
+        -l|--local) URL_BASE=http://127.0.0.1:9000;;
+
+        -u|--up*) WEB_UPLOAD=1;;
+        -c|--check) check_zips;    RET=$?; echo "Exiting ... $RET"; exit $RET;;
+        -i|--index) rebuild_index; RET=$?; echo "Exiting ... $RET"; exit $RET;;
+
+    esac
+    shift
+done
+
+#rebuild_index
+create_zips
+
+if [ $WEB_UPLOAD -ne 0 ];then
+    upload_zips
+    check_zips
+fi
+#exit 0
+
+#exit 0
+
+
+
