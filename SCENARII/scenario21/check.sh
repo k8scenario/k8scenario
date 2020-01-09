636cb66826c7b80e77a79026c602584233fffe99
Michael Bright <1880109+mjbright@users.noreply.github.com>
2020-01-09 09:30:43 -0300

    Added various improvements
    More efficient loop (within test Pod)
    
    More efficient shell commands - use kubectl custom-columns rather than grep/awk/sed/tail/head

--- SCENARII/scenario21/check.sh
+++ SCENARII/scenario21/check.sh
@@ -2,26 +2,54 @@
 
 # kubectl -n k8scenario delete pod/scen3test
 
-PORT=$(kubectl -n k8scenario get service/ckad-demo | awk '/:/ { FS=":"; $0=$5; FS="/"; $0=$2; print $1; }')
+# Original: require extra shell procs:
+#PORT=$(kubectl -n k8scenario get service/ckad-demo | awk '/:/ { FS=":"; $0=$5; FS="/"; $0=$2; print $1; }')
 #CLUSTER_IP=$(kubectl -n k8scenario get service/ckad-demo | awk '/:/ { print $3; }')
-#POD_IP=$(kubectl -n k8scenario get pods --no-headers -o wide | awk '{ print $6; exit(0); }')
+
+# Lighterweight:
+PORT=$(kubectl -n k8scenario get service/ckad-demo -o custom-columns=NODEPORT:.spec.ports[0].nodePort --no-headers)
+#CLUSTER_IP=$(kubectl -n k8scenario get service/ckad-demo -o custom-columns=CLUSTERIP:.spec.clusterIP --no-headers)
+#POD_IPS=$(kubectl -n k8scenario get pods --no-headers -o custom-columns=CLUSTERIP:.status.podIP)
+
+#POD_IP=$(kubectl -n k8scenario get pods --no-headers -o wide | grep ckad-demo | awk '{ print $6; exit(0); }')
 
 # Any node would do:
-NODE_IP=$(kubectl -n k8scenario get pods --no-headers -o wide | awk '{ print $7; exit(0); }')
+# POD_NODE=$(kubectl -n k8scenario get pods --no-headers -o wide | awk '{ print $7; exit(0); }')
+ANY_NODE_IP=$(kubectl -n k8scenario get pods ckad-demo-6fdb684968-hq8qs -o custom-columns=IP:.status.hostIP --no-headers)
 
-EP=${NODE_IP}:${PORT}
+# Could have done this - but extra process:
+# ANY_NODE_IP=$(kubectl -n k8scenario get nodes -o custom-columns=IP:.status.addresses[0].address --no-headers | head -1)
+
+EP=http://${ANY_NODE_IP}:${PORT}/1
 
 LOOPS=10
 
-for i in $(seq $LOOPS); do
-    echo "[loop $i] Checking endpoint $EP"
-    #kubectl run --restart=Never --rm -n k8scenario -it --generator=run-pod/v1 --image=alpine:latest ${NS}-test -- /bin/sh -c "set -x; wget -O - --timeout 4 $EP; RET=\$?; exit \$RET"
-    kubectl run --restart=Never --rm -n k8scenario -it --generator=run-pod/v1 --image=alpine:latest ${NS}-test -- /bin/sh -c "wget -O - --timeout 4 $EP; exit \$?"
+VERSION1() {
+    for i in $(seq $LOOPS); do
+        echo "[loop $i/$LOOPS] Checking endpoint <$EP>"
+        kubectl run --restart=Never --rm -n k8scenario -it --generator=run-pod/v1 --image=alpine:latest k8scenario-test -- /bin/sh -c "wget -qO - --timeout 4 $EP; exit \$?"
 
-    # Bail out on single failure
-    [ $? -ne 0 ] && { echo "Error seen on loop $i"; exit 1; }
-done
+        # Bail out on single failure
+        [ $? -ne 0 ] && { echo "Error seen on loop $i/$LOOPS"; exit 1; }
+    done
+}
 
+VERSION2() {
+    # Faster detection on pass case by looping in container:
+    echo "Checking endpoint <$EP> ... (for $LOOPS loops) "
+
+    ## Bail out on single failure
+    #kubectl run --restart=Never --rm -n k8scenario -it --generator=run-pod/v1 --image=alpine:latest k8scenario-test -- /bin/sh -c "for i in \$(seq 10); do echo loop\$i/$LOOPS; wget -qO - --timeout 4 $EP; [ \$? -ne 0 ] && { echo \"Error seen on loop \$i\"; exit 1; }; done"
+    kubectl run --restart=Never --rm -n k8scenario -it --generator=run-pod/v1 --image=alpine:latest k8scenario-test -- /bin/sh -c "\
+	    for i in \$(seq 10); do
+		    echo -n \"loop\$i/$LOOPS: \";
+		    wget -qO - --timeout 4 $EP;
+		    [ \$? -ne 0 ] && { echo \"Error seen on loop \$i\"; exit 1; };
+	    done
+	    "
+}
+
+VERSION2
 echo "No errors seen on $LOOPS loops"
 exit 0
 
