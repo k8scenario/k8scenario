846d6d5714baf2d3a0cf4c6b7f8bf94d3f90a4fa
Michael Bright <1880109+mjbright@users.noreply.github.com>
2020-01-22 06:40:17 +0100

    Now check/yaml 'sleep 10' command has been changed

--- SCENARII/scenario3/CHECK_SCENARIO.sh
+++ SCENARII/scenario3/CHECK_SCENARIO.sh
@@ -1,3 +1,16 @@
 
-CHECK_PODS_PREFIXED "critical"
+set -x
+# Check 'critical*' Pod is Running
+# Check sleep has been changed to other than 'sleep 10'
+kubectl get pods -o yaml | grep -qE "'sleep *10([^\d]|$)"
+if [ $? -eq 0 ];then
+    exit 1
+else
+    exit 0
+fi
+
+
+#CHECK_PODS_PREFIXED "critical" && 
+#    kubectl get pods -o yaml | grep -E "'sleep *10([^\d]|$)" &&
+#        exit 1
 
