8dafba1d0f2dc65d67b2d787c82a208d395d2878
Michael Bright <1880109+mjbright@users.noreply.github.com>
2020-01-22 06:45:49 +0100

    Removed NodePort check

--- SCENARII/scenario20/CHECK_SCENARIO.sh
+++ SCENARII/scenario20/CHECK_SCENARIO.sh
@@ -4,10 +4,10 @@ SVC_NAME=ckad-demo
 
 # To test ClusterIP:
 CHECK_SVC_CLUSTERIP $SVC_NAME || exit 1
-CHECK_SVC_NODEPORT  $SVC_NAME || exit 1
+#CHECK_SVC_NODEPORT  $SVC_NAME || exit 1
 
-#TEST_POD_SHELL tester "set -x; wget -O - --timeout 4 $ENDPOINT; RET=\$?; exit \$RET"
-#TEST_POD_SHELL tester
-#kubectl -n k8scenario delete pods/tester
+#TEST_POD_SHELL $TESTPOD "set -x; wget -O - --timeout 4 $ENDPOINT; RET=\$?; exit \$RET"
+#TEST_POD_SHELL $TESTPOD
+#kubectl -n k8scenario delete pods/$TESTPOD
 
 
