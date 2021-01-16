8871b3190862fb8304807b175db222bcb8d5ec6b
Michael Bright <1880109+mjbright@users.noreply.github.com>
2021-01-16 12:22:47 +0100

    Added USAGE info

--- scripts/switch_context.sh
+++ scripts/switch_context.sh
@@ -2,6 +2,13 @@
 
 SAVED=~/.k8scenario.saved.context
 
+# USAGE: switch_context.sh
+# - if current context is 'k8scenario' AND file $SAVED (above) is present, put back the saved context
+# - else
+#   - create k8scenario context using namespace k8scenario (assumes KIND cluster)
+#   - set context to k8scenario
+
+
 use_k8scenario_context() {
     kubectl config set-context k8scenario --cluster kind-kind --user kind-kind --namespace=k8scenario
     kubectl config use-context k8scenario
